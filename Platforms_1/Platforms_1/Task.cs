c320f273969f98b381b9a8c29e44f393a97da742
GreatASura <onelonelyflight@gmail.com>
2018-09-20 14:24:59 +0300

    TestsUpv2+ task2

--- Platforms_1/Platforms_1/Task.cs
+++ Platforms_1/Platforms_1/Task.cs
@@ -44,9 +44,11 @@
 
             Dictionary<string, List<string>> contactsContainer = this.GroupArrayListContacts(list);
 
-            this.PrintContactsDictionaryInFIle("File2.txt", contactsContainer);
+            this.WriteContactsDictionaryInFIle("File2.txt", contactsContainer);
 
-            this.PrintNamesWithOnlyPhonesInFile("File3.txt", list);
+            List<string> userNamesHasOnlyPhones = this.UsersWithOnlyPhones(list);
+
+            this.WriteListInFile("File3.txt", userNamesHasOnlyPhones);
         }
 
         /// <summary>
@@ -125,7 +127,7 @@
         {
             var groupList = from Contact l in list
                             group l by (l as Contact).Name;
-            
+
             Dictionary<string, List<string>> groupContacts = new Dictionary<string, List<string>>();
             foreach (IGrouping<string, Contact> g in groupList)
             {
@@ -157,7 +159,7 @@
         /// </summary>
         /// <param name="path"> Path for file writing.</param>
         /// <param name="pairs"> Pairs collection: name - phone number + mail + skype.</param>
-        private void PrintContactsDictionaryInFIle(string path, Dictionary<string, List<string>> pairs)
+        private void WriteContactsDictionaryInFIle(string path, Dictionary<string, List<string>> pairs)
         {
             if (!path.EndsWith(".txt"))
             {
@@ -180,45 +182,43 @@
         }
 
         /// <summary>
-        /// Print in file names of users who has only phones.
+        /// Creates List with name of users who has only phone contact.
         /// </summary>
-        /// <param name="path"> Path for file writing.</param>
         /// <param name="list"> List of contacts.</param>
         /// <returns>
         /// Returns list of names of users who has only phone contact.
         /// </returns>
-        private List<string> PrintNamesWithOnlyPhonesInFile(string path, ArrayList list)
+        private List<string> UsersWithOnlyPhones(ArrayList list)
+        {
+            List<string> namesOnlyWithPhones = (from Contact l in list
+                                                group l by (l as Contact).Name into g
+                                                where g.Count() == 1
+                                                from t in g
+                                                where t is PhoneContact
+                                                select (t as PhoneContact).Name).ToList();
+
+            return namesOnlyWithPhones;
+        }
+
+        /// <summary>
+        /// Print in file List of strings.
+        /// </summary>
+        /// <param name="path"> Path for file writing.</param>
+        /// <param name="list"> List of strings.</param>
+        private void WriteListInFile(string path, List<string> list)
         {
             if (!path.EndsWith(".txt"))
             {
                 throw new Exception("Unknown text file format!");
             }
 
-            var group = from Contact l in list
-                                      group l by (l as Contact).Name into g
-                                      where g.Count() == 1
-                                      select g;
-            List<string> namesOnlyWithPhones = new List<string>();
-            foreach (IGrouping<string, Contact> g in group)
-            {
-                foreach (var t in g)
-                {
-                    if (t is PhoneContact)
-                    {
-                        namesOnlyWithPhones.Add((t as PhoneContact).Name);
-                    }
-                }
-            }
-
             using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
             {
-                foreach (string n in namesOnlyWithPhones)
+                foreach (string l in list)
                 {
-                    sw.WriteLine(n);
+                    sw.WriteLine(l);
                 }
             }
-
-            return namesOnlyWithPhones;
         }
     }
 }

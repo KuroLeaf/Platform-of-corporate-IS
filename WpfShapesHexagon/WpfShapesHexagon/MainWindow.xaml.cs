36c2b8a03c693d514c7ad14116095fc16592fb5f
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2018-10-09 21:42:49 +0300

    Add new project WpfShapesHexagon(creating MainWindow)

---
+++ WpfShapesHexagon/WpfShapesHexagon/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿namespace WpfShapesHexagon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Main Logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {}
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {}
+
+        private void MoveMode_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void DrawMode_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void CanvasArea_MouseMove(object sender, MouseEventArgs e)
+        { }
+
+        private void CanvasArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        { }
+
+        private void MouseDownShape(object sender, MouseButtonEventArgs e)
+        {}
+
+        private void MouseMoveShape(object sender, MouseEventArgs e)
+        {}
+
+        private void MouseUpShape(object sender, MouseButtonEventArgs e)
+        {}
+    }
+}
+

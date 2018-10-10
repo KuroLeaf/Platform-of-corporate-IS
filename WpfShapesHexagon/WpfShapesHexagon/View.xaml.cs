69ed59c171b2f0ec0a0455dad8784c308070afd4
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2018-10-10 23:05:08 +0300

    Adding Functions
    #10

--- WpfShapesHexagon/WpfShapesHexagon/View.xaml.cs
+++ WpfShapesHexagon/WpfShapesHexagon/View.xaml.cs
@@ -3,20 +3,15 @@
 // </copyright>
 namespace WpfShapesHexagon
 {
-    using System;
-    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Microsoft.Win32;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
     using System.Windows.Shapes;
+    using System.Windows;
+    using System.Windows.Input;
+   
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -35,14 +30,49 @@ namespace WpfShapesHexagon
             DataContext = vm;
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {}
+        private void Save_as_Click(object sender, RoutedEventArgs e)
+        {
+            string _path = "File.xml";
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.DefaultExt = ".xml";
+            if (dialog.ShowDialog() == true)
+            {
+                _path = dialog.FileName;
+            }
+            vm.Serialize(_path);
+        }
 
         private void Open_Click(object sender, RoutedEventArgs e)
-        {}
+        {
+            if (sender == null)
+            {
+                throw new System.ArgumentNullException(nameof(sender));
+            }
+
+            string _path = "File.xml";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".xml";
+            if (dialog.ShowDialog() == true)
+            {
+                _path = dialog.FileName;
+            }
+            vm.Deserialize(_path);
+        }
 
         private void New_Click(object sender, RoutedEventArgs e)
-        {}
+        {
+            if (e == null)
+            {
+                throw new System.ArgumentNullException(nameof(e));
+            }
+
+            if (isDrawing)
+            {
+                vm.Polygones.RemoveAt(vm.Count - 1);
+                isDrawing = false;
+            }
+            vm.Polygones = new ObservableCollection<Polygon>();
+        }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
@@ -65,10 +95,45 @@ namespace WpfShapesHexagon
         }
 
         private void CanvasArea_MouseMove(object sender, MouseEventArgs e)
-        { }
+        {
+            vm.YPos = (int)Mouse.GetPosition(sender as UIElement).Y;
+            vm.XPos = (int)Mouse.GetPosition(sender as UIElement).X;
+
+            if (vm.Mode == MovingDrawingEnum.Drawing.ToString() && isDrawing)
+            {
+                vm.Change_Last(new Point(vm.XPos, vm.YPos));
+            }
+        }
 
         private void CanvasArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        { }
+        {
+            if (vm.Mode == MovingDrawingEnum.Drawing.ToString())
+            {
+                if (!isDrawing)
+                {
+                    isDrawing = true;
+                    vm.Add(new Polygon() { Points = new PointCollection() { new Point(vm.XPos, vm.YPos), new Point(vm.XPos, vm.YPos) }, Fill = Brushes.LightGoldenrodYellow, Stroke = Brushes.Black });
+                }
+                else
+                {
+                    vm.Add_Last(new Point(vm.XPos, vm.YPos));
+                    if (vm.Count_Last >= 7)
+                    {
+                        ColorDialog colorDialog = new ColorDialog();
+                        colorDialog.Owner = this;
+                        colorDialog.fillColorPicker.SelectedColor = (vm.Polygones.Last().Fill as SolidColorBrush).Color;
+                        colorDialog.strokeColorPicker.SelectedColor = (vm.Polygones.Last().Stroke as SolidColorBrush).Color;
+                        colorDialog.ShowDialog();
+                        if (colorDialog.DialogResult == true)
+                        {
+                            vm.Polygones.Last().Fill = new SolidColorBrush((Color)colorDialog.fillColorPicker.SelectedColor);
+                            vm.Polygones.Last().Stroke = new SolidColorBrush((Color)colorDialog.strokeColorPicker.SelectedColor);
+                        }
+                        isDrawing = false;
+                    }
+                }
+            }
+        }
 
         private void MouseDownShape(object sender, MouseButtonEventArgs e)
         {

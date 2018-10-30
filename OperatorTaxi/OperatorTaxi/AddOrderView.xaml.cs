57fc10d2059d51994c09bc7e4971a45cf4f8b8fc
GreatASura <onelonelyflight@gmail.com>
2018-10-30 14:43:17 +0300

    AddedView #18

--- OperatorTaxi/OperatorTaxi/AddOrderView.xaml.cs
+++ OperatorTaxi/OperatorTaxi/AddOrderView.xaml.cs
@@ -22,6 +22,31 @@ namespace OperatorTaxi
         public AddOrderView()
         {
             InitializeComponent();
+          
+            {
+                InitializeComponent();
+            }
+            
+        }
+        private void AddOrder(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)App.Current.MainWindow;
+            if (this.startPoint.Text != "" && this.endPoint.Text != "" && this.count.Text != "")
+            {
+                try
+                {
+                    mainWindow.a.Orders.Add(new Order(this.startPoint.Text, this.endPoint.Text, Convert.ToInt32(this.count.Text), status.NotAppointed, "------"));
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Неправильно введені дані");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ви не ввели всі дані");
+            }
         }
     }
 }

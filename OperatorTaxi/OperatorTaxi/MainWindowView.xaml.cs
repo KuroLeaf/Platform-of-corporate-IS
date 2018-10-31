0754b4a2444304b6453386d323356d93053ebfaf
GreatASura <onelonelyflight@gmail.com>
2018-10-31 11:34:28 +0300

    mwv

--- OperatorTaxi/OperatorTaxi/MainWindowView.xaml.cs
+++ OperatorTaxi/OperatorTaxi/MainWindowView.xaml.cs
@@ -21,9 +21,45 @@ namespace OperatorTaxi
     /// </summary>
     public partial class MainWindow : Window
     {
+        public TaxiView Taxi;
+        public AddOrderView addOrder;
+        public MainWindowViewModel vm;
+
         public MainWindow()
         {
             InitializeComponent();
+            vm = new MainWindowViewModel("Orders.txt", "Taxists.txt");
+            this.DataContext = vm;
         }
+
+        private void Taxists(object sender, RoutedEventArgs e)
+        {
+            Taxi = new TaxiView();
+            Taxi.DataContext = vm;
+            Taxi.Show();
+        }
+
+        private void AddOrder(object sender, RoutedEventArgs e)
+        {
+            addOrder = new AddOrderView();
+            addOrder.Show();
+        }
+
+        private void DeleteOrder(object sender, RoutedEventArgs e)
+        {
+            vm.DeleteOrder();
+
+        }
+
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            vm.Save();
+        }
+
+        private void UploadData(object sender, RoutedEventArgs e)
+        {
+            vm.Upload();
+        }
+
     }
 }

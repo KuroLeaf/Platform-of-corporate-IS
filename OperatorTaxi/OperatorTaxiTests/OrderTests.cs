b55f84d7a382416a2f233fd8119fe378d7abdf07
mdzhura <32909428+mdzhura@users.noreply.github.com>
2018-11-01 05:06:52 +0200

    Created unit test module and added tests for Taxist entity

---
+++ OperatorTaxi/OperatorTaxiTests/OrderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OperatorTaxiTests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void TestOrder()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}
---
+++ OperatorTaxi/OperatorTaxiTests/ViewTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OperatorTaxiTests
+{
+    [TestClass]
+    public class ViewTests
+    {
+        [TestMethod]
+        public void TestView()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}

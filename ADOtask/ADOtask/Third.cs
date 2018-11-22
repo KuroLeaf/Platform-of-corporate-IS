8e89a8dbeaa97ca527cb85598cffb4eb7bdae67d
GreatASura <onelonelyflight@gmail.com>
2018-11-22 21:59:07 +0200

    #25 third group final

--- ADOtask/ADOtask/Third.cs
+++ ADOtask/ADOtask/Third.cs
@@ -7,39 +7,37 @@ namespace ADOtask
     {
         public static void ThirdGroup(SqlConnection _sqlConnection)
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Third group of task");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nSTART OF THIRD PART!\n");
             SqlCommand command;
             SqlDataReader reader;
-            //22
-            Console.WriteLine("25. Show the list of french customers’ names who used to order french products (use left join).");
+            Console.WriteLine("24. Show the list of french customers’ names who used to order french products.");
             string command1 =
-                 "SELECT cust.Country, SUM(ordd.Quantity) AS TotalSum FROM Customers AS cust, Orders AS ord, [Order Details] AS ordd WHERE ord.CustomerID = cust.CustomerID AND ord.OrderID = ordd.OrderID GROUP BY cust.Country; ";
+               "SELECT DISTINCT ContactName " +
+               "FROM Customers, Orders " +
+               "WHERE Customers.CustomerID = Orders.CustomerID AND Country = 'France' AND ShipCountry = 'France'";
             command = new SqlCommand(command1, _sqlConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
+            reader.Close();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Third group of task");
+           
+           
+            Console.WriteLine("25. Show the total ordering sum calculated for each country of customer.");
+            string command2 =
+                 "SELECT cust.Country, SUM(ordd.Quantity) AS TotalSum FROM Customers AS cust, Orders AS ord, [Order Details] AS ordd WHERE ord.CustomerID = cust.CustomerID AND ord.OrderID = ordd.OrderID GROUP BY cust.Country; ";
+            command = new SqlCommand(command2, _sqlConnection);
             reader = command.ExecuteReader();
             while (reader.Read())
             {
                 Console.WriteLine("{0,-10}{1}", reader["Country"], reader["TotalSum"]);
             }
             reader.Close();
-          
-           
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            //23
-            Console.WriteLine("23. Show the list of french customers’ names who used to order french products.");
-            string command2 =
-               "SELECT DISTINCT ContactName " +
-               "FROM Customers, Orders " +
-               "WHERE Customers.CustomerID = Orders.CustomerID AND Country = 'France' AND ShipCountry = 'France'";
-            command = new SqlCommand(command2, _sqlConnection);
-            reader = command.ExecuteReader();
-           // Console.WriteLine("Company Name");
-            while (reader.Read())
-            {
-                Console.WriteLine(reader[0]);
-            }
-            reader.Close();
+            Console.WriteLine("\nEND OF THIRD PART!\n");
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     }

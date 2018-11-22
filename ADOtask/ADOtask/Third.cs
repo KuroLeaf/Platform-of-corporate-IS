9b36ec5579191adb045f8218a7d63ac78f8a17b0
GreatASura <onelonelyflight@gmail.com>
2018-11-22 21:53:11 +0200

    sql fix 3group

--- ADOtask/ADOtask/Third.cs
+++ ADOtask/ADOtask/Third.cs
@@ -12,31 +12,29 @@ namespace ADOtask
             SqlCommand command;
             SqlDataReader reader;
             //22
-            Console.WriteLine("22. Show the list of french customers’ names who used to order french products (use left join).");
+            Console.WriteLine("25. Show the list of french customers’ names who used to order french products (use left join).");
             string command1 =
-                "SELECT ContactName " +
-                "FROM Customers " +
-                "LEFT JOIN Orders on Customers.CustomerID = Orders.CustomerID AND ShipCountry = 'France' " +
-                "WHERE Country = 'France' AND ShipCountry IS NOT NULL GROUP BY ContactName";
+                 "SELECT cust.Country, SUM(ordd.Quantity) AS TotalSum FROM Customers AS cust, Orders AS ord, [Order Details] AS ordd WHERE ord.CustomerID = cust.CustomerID AND ord.OrderID = ordd.OrderID GROUP BY cust.Country; ";
             command = new SqlCommand(command1, _sqlConnection);
             reader = command.ExecuteReader();
-
-            Console.WriteLine("Result:");
             while (reader.Read())
             {
-                Console.WriteLine(reader[0]);
+                Console.WriteLine("{0,-10}{1}", reader["Country"], reader["TotalSum"]);
             }
             reader.Close();
+          
+           
 
+            Console.ForegroundColor = ConsoleColor.Green;
             //23
             Console.WriteLine("23. Show the list of french customers’ names who used to order french products.");
             string command2 =
-               "SELECT DISTINCT CompanyName " +
+               "SELECT DISTINCT ContactName " +
                "FROM Customers, Orders " +
                "WHERE Customers.CustomerID = Orders.CustomerID AND Country = 'France' AND ShipCountry = 'France'";
             command = new SqlCommand(command2, _sqlConnection);
             reader = command.ExecuteReader();
-            Console.WriteLine("Company Name");
+           // Console.WriteLine("Company Name");
             while (reader.Read())
             {
                 Console.WriteLine(reader[0]);

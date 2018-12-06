458cc1852e993390fd3a4bfa1f59c5c473b126f2
SergiyMTVLviv <43314832+SergiyMTVLviv@users.noreply.github.com>
2018-12-06 15:15:30 +0200

    Add Second class to project
    #26

--- ADOtask/ADOtask/Second.cs
+++ ADOtask/ADOtask/Second.cs
@@ -8,7 +8,56 @@ namespace ADOtask
     {
         public static void SecondGroup(SqlConnection _sqlConnection)
         {
+            Console.WriteLine("Second group of task");
 
+            string command1 =
+                "SELECT CompanyName " +
+                "FROM Customers LEFT JOIN Orders on Customers.CustomerID = Orders.CustomerID AND ShipCountry = 'France' " +
+                "WHERE Country = 'France' AND ShipCountry IS NOT NULL GROUP BY CompanyName";
+
+            string command2 =
+                "SELECT ContactName " +
+                "FROM Customers " +
+                "WHERE CustomerID IN " +
+                "(SELECT CustomerID FROM Orders WHERE OrderID IN " +
+                "(SELECT OrderID FROM[Order Details] WHERE ProductID IN " +
+                "(SELECT ProductID FROM Products WHERE ProductName = 'Tofu')))";
+
+            SqlCommand command;
+            SqlDataReader reader;
+
+            // 22
+            Console.WriteLine("22. Show the list of french customers’ names who used to order french products (use left join).");
+
+            command = new SqlCommand(command1, _sqlConnection);
+            reader = command.ExecuteReader();
+
+            Console.WriteLine("Company Name");
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
+            reader.Close();
+
+            //20
+            Console.WriteLine("20. Show the list of customers’ names who used to order the ‘Tofu’ product.");
+
+            command = new SqlCommand(command2, _sqlConnection);
+            reader = command.ExecuteReader();
+
+            Console.WriteLine("Result:");
+            Console.WriteLine("  {0,10}", reader.GetName(0));
+            while (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("  {0,10}", reader.GetString(0));
+                }
+                reader.NextResult();
+            }
+
+            Console.WriteLine();
+            reader.Close();
         }
     }
 }

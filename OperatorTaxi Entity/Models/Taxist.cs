19262ee746f3a444062c1c31ba13aa391597d881
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2018-12-13 22:38:28 +0200

    Add OperatorTaxi without used Entity
    #29

---
+++ OperatorTaxi Entity/Models/Taxist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Models
+{
+    public class Taxist
+    {
+        public Taxist() { }
+
+
+        public Taxist(string model, string number, bool isBusy)
+        {
+            this.Model = model;
+            this.Number = number;
+            this.IsBusy = isBusy;
+        }
+
+        public string Model { get; set; }
+        public string Number { get; set; }
+        public bool IsBusy { get; set; }
+    }
+}

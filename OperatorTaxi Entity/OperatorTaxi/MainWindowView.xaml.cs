9f0216e9905d382b8d81e60c911bc8bd492fd62d
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2018-12-14 22:57:01 +0200

    Add Interface Repository realization UoW.
    Co-Authored-By: mdzhura <mdzhura@users.noreply.github.com>
    
    #31 #32

--- OperatorTaxi Entity/OperatorTaxi/MainWindowView.xaml.cs
+++ OperatorTaxi Entity/OperatorTaxi/MainWindowView.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using OperatorTaxi.ViewModels;
-
-namespace OperatorTaxi
+﻿namespace OperatorTaxi
 {
+    using OperatorTaxi.ViewModels;
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -69,11 +57,6 @@ namespace OperatorTaxi
         private void Exit(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
-        }
-
-        private void orderListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }

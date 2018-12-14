7e41e0229e5017be36b7d77fd1db85739dd88221
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2018-12-14 23:10:11 +0200

    Realization of Repository.
    Co-Authored-By: mdzhura <mdzhura@users.noreply.github.com>

--- OperatorTaxi Entity/OperatorTaxi/Models/OperatorContext.cs
+++ OperatorTaxi Entity/OperatorTaxi/Models/OperatorContext.cs
@@ -2,7 +2,7 @@
 {
     using System.Data.Entity;
 
-    class OperatorContext: DbContext
+    public class OperatorContext: DbContext
     {
         public DbSet<Order> Orders { get; set; }
         public DbSet<Taxist> Taxists { get; set; }
